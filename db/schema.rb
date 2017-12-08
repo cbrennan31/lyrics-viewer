5e0cd4d78b93b16a08ed958b00fe7f660ae7d8df
cbrennan31 <31743841+cbrennan31@users.noreply.github.com>
2017-12-08 14:57:51 -0500

    fix database

--- db/schema.rb
+++ db/schema.rb
@@ -15,65 +15,25 @@ ActiveRecord::Schema.define(version: 20171207230129) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "events", id: :serial, force: :cascade do |t|
+  create_table "events", force: :cascade do |t|
     t.string "title", null: false
     t.datetime "time", null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "games", id: :serial, force: :cascade do |t|
-    t.integer "total_score", null: false
-    t.integer "questions_correct", null: false
-    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "songs", id: :serial, force: :cascade do |t|
+  create_table "songs", force: :cascade do |t|
     t.string "title"
     t.integer "event_id", null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "user_clues", id: :serial, force: :cascade do |t|
-    t.string "question", null: false
-    t.string "answer", null: false
-    t.integer "value", null: false
-    t.string "category", null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer "user_game_id", null: false
-  end
-
-  create_table "user_games", id: :serial, force: :cascade do |t|
-    t.string "title", null: false
-    t.string "description", null: false
-    t.integer "strikes", null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer "user_id", null: false
-  end
-
-  create_table "users", id: :serial, force: :cascade do |t|
-    t.string "provider"
-    t.string "uid"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "token"
-    t.string "refresh_token"
-    t.datetime "oauth_expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "verses", id: :serial, force: :cascade do |t|
+  create_table "verses", force: :cascade do |t|
     t.integer "song_id", null: false
     t.text "lyrics", null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.boolean "current", default: false
   end
 
