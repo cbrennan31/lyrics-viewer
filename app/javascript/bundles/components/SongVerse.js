2f3031d2b8bb53623aa5d508407d2574c868370f
cbrennan31 <31743841+cbrennan31@users.noreply.github.com>
2018-01-04 00:19:01 -0500

    add new line to verse by hitting enter in textarea

--- app/javascript/bundles/components/SongVerse.js
+++ app/javascript/bundles/components/SongVerse.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const SongVerse = (props) => {
   let fontColor = props.selected ? 'red' : 'black'
+  let lyricsArray = props.lyrics.split("<br />")
+  let displayLyrics = lyricsArray.map((lyric) => {
+    if (lyricsArray.indexOf(lyric) != lyricsArray.length - 1) {
+      return <div><span>{lyric}</span><br /></div>
+    } else {
+      return <span>{lyric}</span>
+    }
+  })
+
   return(
-    <div style = {{color: fontColor}}>{props.lyrics}</div>
+    <div style = {{color: fontColor}}>{displayLyrics}</div>
   )
 }
 
