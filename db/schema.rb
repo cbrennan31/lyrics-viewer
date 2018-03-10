320332b00aee246387f5fdc91ddf22c4c47d3693
cbrennan31 <31743841+cbrennan31@users.noreply.github.com>
2018-03-10 13:45:51 -0500

    add clearance gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180214174253) do
+ActiveRecord::Schema.define(version: 20180310180325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20180214174253) do
     t.datetime "updated_at", null: false
     t.boolean "in_progress", default: false
     t.integer "selected_song_id"
+    t.integer "user_id", null: false
   end
 
   create_table "songs", force: :cascade do |t|
@@ -30,6 +31,17 @@ ActiveRecord::Schema.define(version: 20180214174253) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "selected_verse_id", default: 0
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", limit: 128, null: false
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128, null: false
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
   create_table "verses", force: :cascade do |t|
